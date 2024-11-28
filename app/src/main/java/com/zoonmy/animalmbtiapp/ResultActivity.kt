dbcc7c90b96004cee1064125789934b64b0db695
jinii9 <jinii915@naver.com>
2024-11-28 11:21:32 +0900

    markup: 결과레이아웃 & 결과전체조회레이아웃

--- app/src/main/java/com/zoonmy/animalmbtiapp/ResultActivity.kt
+++ app/src/main/java/com/zoonmy/animalmbtiapp/ResultActivity.kt
@@ -6,10 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result)
 
     }
 }
