46d6708607fd08332427dbcb60240c83c24ea45d
junsoo1003 <royalex@dgu.ac.kr>
2024-11-28 12:00:10 +0900

    QuizActivity

--- app/src/main/java/com/zoonmy/animalmbtiapp/MainActivity.kt
+++ app/src/main/java/com/zoonmy/animalmbtiapp/MainActivity.kt
@@ -1,22 +1,35 @@
 package com.zoonmy.animalmbtiapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var getResult: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        getResult = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                val data = result.data!!.getStringExtra("resultKey")
+                // 결과 처리
+            }
+        }
 
         val startButton = findViewById<Button>(R.id.startButton)
 
         startButton.setOnClickListener {
-
+            val intent = Intent(this, QuizActivity::class.java)
+            getResult.launch(intent) // QuizActivity 실행
         }
     }
 }
