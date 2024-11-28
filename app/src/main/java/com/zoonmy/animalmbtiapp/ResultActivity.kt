37f1d96a3428e54901788d4964c19e31e7306728
jinii9 <jinii915@naver.com>
2024-11-28 11:28:57 +0900

    feat: MainActivity-AllResultsActivity 로직 연결

--- app/src/main/java/com/zoonmy/animalmbtiapp/ResultActivity.kt
+++ app/src/main/java/com/zoonmy/animalmbtiapp/ResultActivity.kt
@@ -1,16 +1,20 @@
 package com.zoonmy.animalmbtiapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_result)
+
+        val buttonLook = findViewById<Button>(R.id.buttonLook)
+        buttonLook.setOnClickListener {
+            startActivity(Intent(this, AllResultsActivity::class.java))
+        }
 
     }
 }
