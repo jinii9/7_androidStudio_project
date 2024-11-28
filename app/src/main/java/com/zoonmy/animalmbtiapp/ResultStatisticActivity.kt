ef8ce8c2a258743d7809442d413e8a231b5844fb
zoonmy <pjm2571@naver.com>
2024-11-28 14:43:37 +0900

    refactor: textView 추가 수정

--- app/src/main/java/com/zoonmy/animalmbtiapp/ResultStatisticActivity.kt
+++ app/src/main/java/com/zoonmy/animalmbtiapp/ResultStatisticActivity.kt
@@ -7,6 +7,25 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ResultStatisticActivity : AppCompatActivity() {
+    private val textViewIds = listOf(
+        R.id.enfpTextView,
+        R.id.entpTextView,
+        R.id.enfjTextView,
+        R.id.entjTextView,
+        R.id.infpTextView,
+        R.id.intpTextView,
+        R.id.infjTextView,
+        R.id.intjTextView,
+        R.id.esfpTextView,
+        R.id.estpTextView,
+        R.id.esfjTextView,
+        R.id.estjTextView,
+        R.id.isfpTextView,
+        R.id.istpTextView,
+        R.id.isfjTextView,
+        R.id.istjTextView
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result_statistic)
