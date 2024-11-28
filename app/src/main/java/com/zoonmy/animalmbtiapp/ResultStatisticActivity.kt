3dbd59a4e2417d1b88dd7c281d4173ebc7c0615d
zoonmy <pjm2571@naver.com>
2024-11-28 14:38:37 +0900

    refactor: textView 이름 수정

--- app/src/main/java/com/zoonmy/animalmbtiapp/ResultStatisticActivity.kt
+++ app/src/main/java/com/zoonmy/animalmbtiapp/ResultStatisticActivity.kt
@@ -1,6 +1,7 @@
 package com.zoonmy.animalmbtiapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +13,8 @@ class ResultStatisticActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_result_statistic)
 
+
+        val enfpTextView = findViewById<TextView>(R.id.enfpTextView)
+        val entpTextView = findViewById<TextView>(R.id.entpTextView)
     }
 }
